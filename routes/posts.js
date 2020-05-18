5e1caa291512c3a3395d99129bd0f5964e8cd78a
uDivy <upadhyaydivy10@gmail.com>
2020-05-18 19:48:48 +0530

    Adding Pagination

--- routes/posts.js
+++ routes/posts.js
@@ -85,16 +85,30 @@ router.put("/:id", multer({storage: storage}).single("image"),
 
 router.get("",
     (req, res, next) => {
-        Post.find().then(
+        const pageSize = +req.query.pagesize;
+        const currentPage = +req.query.page;
+        const postQuery =  Post.find();
+        let fetchedPosts;
+        if (pageSize && currentPage) {
+            postQuery
+                .skip(pageSize*(currentPage-1))
+                .limit(pageSize);
+        }
+        postQuery.then(
             (documents) => {
+            fetchedPosts = documents;
+            return Post.count()
+            }
+            ).then( (count) => {
                 res.status(200).json(
                     {
                         messages: "Post Sent Successfully!",
-                        posts: documents
+                        posts: fetchedPosts,
+                        maxPosts: count
                     }
-                );
+            )
             }
-        );
+            );
     }
 );
 
